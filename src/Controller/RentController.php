db58076ab55f3c537242e63d6768eda6a390459d
Aleksandar16 <a.milenkovic321@gmail.com>
2023-07-02 10:57:33 +0200

    rentController changements

--- src/Controller/RentController.php
+++ src/Controller/RentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\AdSpace;
+use App\Entity\Rent;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -46,26 +48,33 @@ class RentController extends AbstractController
         $userRepository = $entityManager->getRepository(User::class);
         $superAdmins = $userRepository->findSuperAdmins();
 
+        $data = json_decode($request->getContent(), true);
+
+        $entity1Data = $data['entity1Data'] ?? null;
+
+        if (!isset($entity1Data)) {
+            return $this->json(['error' => 'Entity1Data is missing'], 400);
+        }
+
         foreach ($superAdmins as $admin) {
             $adminEmail = (new Email())
                 ->from('noreply@concoursphoto.com')
                 ->to($admin->getEmail())
-                ->subject($member->getUsername() . ' : nouveau compte membre créé')
+                ->subject(' nouveau compte membre créé')
                 ->html('Un nouveau compte membre a été créé : <br>
 
-                    Pseudo : ' . $member->getUsername() . ' <br>
-                    Genre : ' . $user->getGender() . ' <br>
-                    Prénom : ' . $user->getFirstname() . ' <br>
-                    Nom : ' . $user->getLastname() . ' <br>
-                    Age : ' . $user->getBirthdate()->diff(new \DateTime())->y . ' <br>
-                    Ville : ' . $user->getCity() . ' <br>
-
-                    <a href="http://localhost:8000/admin?crudAction=detail&crudControllerFqcn=App%5CController%5CAdmin%5CUserCrudController&entityId=' . $user->getId() . '">Accéder à la fiche</a>');
+                    Espace de publicité : '. $entityManager->getRepository(AdSpace::class)->find(id: 1)->getName() .' <br>
+                    Date de début : '. $entity1Data['start_date'] .' <br>
+                    Date de fin : '. $entity1Data['end_date'] .' <br>
+                    Visuel : '. $entity1Data['file'] .' <br>
+                    Url : '. $entity1Data['click_url'] .' <br>
+                    Remarques : '. $entity1Data['start_date'] .' <br>');
 
             try {
                 $mailer->send($adminEmail);
             } catch (\Exception $e) {
-                echo $e->getMessage();
+                error_log($e->getMessage());
+                return new JsonResponse(['error' => 'Mail could not be sent'], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
             }
         }
 
@@ -86,60 +95,36 @@ class RentController extends AbstractController
             return new JsonResponse(['error' => 'User not logged in user'], 401);
         }
 
-        $organization = $this->entityManager->getRepository(Organization::class)->find($id);
+        $rent = $this->entityManager->getRepository(Rent::class)->find($id);
 
-        if (!$organization || !$user->getOrganizations()->contains($organization)) {
-            return new JsonResponse(['error' => 'Organization not found or access denied'], 404);
-        }
-
-        if (!$organization) {
-            $organization = new Organization();
-            $user->addOrganization($organization);
-            $this->entityManager->persist($organization);
+        if (!$rent) {
+            return new JsonResponse(['error' => 'Rent not found or access denied'], 404);
         }
 
         $data = json_decode($request->getContent(), true);
 
         $entity1Data = $data['entity1Data'] ?? null;
-        $entity2Data = $data['entity2Data'] ?? null;
 
-        if (!isset($entity1Data) && !isset($entity2Data)) {
-            return $this->json(['error' => 'Both entity1Data and entity2Data are missing'], 400);
-        } elseif (!isset($entity1Data)) {
-            return $this->json(['error' => 'entity1Data is missing'], 400);
-        } elseif (!isset($entity2Data)) {
-            return $this->json(['error' => 'entity2Data is missing'], 400);
+        if (!isset($entity1Data)) {
+            return $this->json(['error' => 'Entity1Data is missing'], 400);
         }
 
         try {
-            $serializer->deserialize(json_encode($entity1Data), Organization::class, 'json', ['object_to_populate' => $organization]);
+            $serializer->deserialize(json_encode($entity1Data), Rent::class, 'json', ['object_to_populate' => $rent]);
 
-            if (array_key_exists('logo', $entity1Data)) {
-                $base64Image = $entity1Data['logo'];
+            if (array_key_exists('file', $entity1Data)) {
+                $base64Image = $entity1Data['file'];
                 if ($base64Image === null) {
-                    $organization->setLogo(null);
+                    $rent->setFile(null);
                 } else {
                     $base64Image = preg_replace('#^data:image/\w+;base64,#i', '', $base64Image);
                     $data = base64_decode($base64Image);
                     $uniqueFileName = uniqid() . '.png';
                     $imagePath = $this->getParameter('uploads_images_directory') . '/' . $uniqueFileName;
                     file_put_contents($imagePath, $data);
-                    $organization->setLogo($uniqueFileName);
+                    $rent->setFile($uniqueFileName);
                 }
             }
-
-            $socialNetwork = $organization->getSocialNetwork();
-
-            if (!$socialNetwork) {
-                $socialNetwork = new SocialNetwork();
-                $organization->setSocialNetwork($socialNetwork);
-                $this->entityManager->persist($socialNetwork);
-            }
-
-            $organization->setUpdateDate(new \DateTime());
-
-            $serializer->deserialize(json_encode($entity2Data), SocialNetwork::class, 'json', ['object_to_populate' => $socialNetwork]);
-
         } catch (\Exception $e) {
             return $this->json([
                 'error' => 'Invalid data provided',
